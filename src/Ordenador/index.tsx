5324016188ab06d9c4172d42eaf513903f8baefe
luizgcj <46451475+luizgcj@users.noreply.github.com>
2022-06-17 17:41:14 -0300

    first commit

---
+++ src/Ordenador/index.tsx
@@ -0,0 +1,44 @@
+import styles from './Ordenador.module.scss';
+import opcoes from './opcoes.json';
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import { MdKeyboardArrowDown, MdKeyboardArrowUp, MdOutlineRedeem} from 'react-icons/md';
+
+export type OpcoesOrdenador = '' | 'porcao' | 'qtd_pessoas' | 'preco';
+
+interface Props {
+    ordenador: OpcoesOrdenador,
+    setOrdenador: React.Dispatch<React.SetStateAction<OpcoesOrdenador>>;
+}
+
+export default function Ordenador({ ordenador, setOrdenador} : Props) {
+    const [aberto, setAberto] = useState(false);
+    const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
+    return (
+        <button 
+           className={classNames({
+              [styles.ordenador] : true,
+              [styles["ordenador--ativo"]] : ordenador !== ""
+           })} 
+           onClick={() => setAberto(!aberto)}
+           onBlur={() => setAberto(false)} //clicar fora irá fechar o popup
+        >
+            <span>{nomeOrdenador || "Ordenador por"}</span>
+            {aberto ? <MdKeyboardArrowUp size={20} /> : <MdKeyboardArrowDown size={20}/>}
+            <div className= {classNames({
+                [styles.ordenador__options] : true,
+                [styles["ordenador__options--ativo"]] : aberto
+            })}
+             >
+                {opcoes.map((opcao) => (
+                    <div className={styles.ordenador__option} 
+                         key={opcao.value}
+                         onClick={() => setOrdenador(opcao.value as OpcoesOrdenador)}
+                    >
+                        {opcao.nome}
+                    </div>
+                ))}
+            </div>
+        </button>
+    )
+}
---
+++ src/pages/Cardapio/Itens/index.tsx
@@ -0,0 +1,70 @@
+import cardapio from './itens.json';
+import Item from './Item';
+import { useEffect, useState } from 'react';
+
+interface Props {
+    busca: string;
+    filtro: number | null;
+    ordenador: string;
+}
+
+export default function Itens(props : Props){
+    const [lista, setLista] = useState(cardapio);
+    const {busca, filtro, ordenador} = props;
+
+    function testaBusca(title : string){
+        const regex = new RegExp(busca, 'i');
+        return regex.test(title);
+    }
+
+    function testaFiltro(id : number){
+        if (filtro !== null) return filtro === id;
+        return true;
+    }
+
+    function ordenar(novaLista : typeof cardapio){       
+         switch(ordenador){
+            case 'porcao':
+                return ordenarPropriedadeCrescente(novaLista, 'size');
+            case 'qtd_pessoas':
+                return ordenarPropriedadeCrescente(novaLista, 'serving');
+            case 'preco':
+                return ordenarPropriedadeCrescente(novaLista, 'price');
+            default:
+                return novaLista;
+        }
+        
+    }
+
+    const ordenarPropriedadeCrescente = (
+        lista: typeof cardapio,
+        propriedade: 'size' | 'serving' | 'price'
+      ) => {
+        return lista.sort((a, b) => (a[propriedade] > b[propriedade] ? 1 : -1));
+      };
+
+
+
+    useEffect(() => {
+        const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+        setLista(ordenar(novaLista));
+    }, [busca, filtro, ordenador])
+
+    return (
+        <div>
+            {lista.map((item)=> (
+                <Item
+                   key={item.id}
+                  /*imagem={item.photo}
+                  descricao={item.description} 
+                  titulo={item.title} 
+                  tipo={item.category.label} 
+                  porcao={item.size}
+                  qtdePessoas={item.serving}
+                  valor={item.price}*/
+                   {...item}
+                  />   
+            ))}
+        </div>
+    )
+}
